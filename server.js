ee8e46ae868dfc80ac3dde734cb76313804dac6e
Andre Macedo Tertuliano <andre.tertuliano@prodaptech.com.br>
2016-06-09 14:14:09 -0300

    commit

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var app = require('./config/express');
-require('./config/database')('mongodb://localhost/alurapic');
 
 http.createServer(app)
 .listen(3000, function() {
